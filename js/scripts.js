8d3a243f3ba30e51d1cdbc8f7999c5423a090c48
cwanjugu <cwanjugu@gmail.com>
2020-10-11 18:42:02 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,3 @@
-// $(document).ready(function() {
-//     $("#blanks form").submit(function() {
-//       alert('Go to beginning of form submit!');
-//       var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
-
-//       blanks.forEach(function(blank) {
-//         var userInput = $("input." + blank).val();
-//         $("." + blank).text(userInput).val();
-//       });
-
-//       $("#story").show();
-//       alert('Go to end of form submit!');
-//     });
-//   });
-
-
-
 $(document).ready(function () {
 $(".design").click(function (){
 $("#designingImg").toggle();
